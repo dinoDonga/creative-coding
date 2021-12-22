e0a9cf6c6f966668810ea4098a5a9d2f6172761c
Leon Gieser <leon.gieser@gmail.com>
2021-12-23 00:01:39 +0100

    perlin noise iterations

--- 002-perlin-noise/sketch.js
+++ 002-perlin-noise/sketch.js
@@ -1,5 +1,5 @@
 let t = 0;
-let q = 50;
+let diameter = 250;
 
 function setup() {
     const canvas = createCanvas(800, 600);
@@ -8,21 +8,36 @@ function setup() {
 
 function draw() {
     background(20, 1);
-    fill(255);
 
     // let x = random(100, width - 100);
     // let y = random(100, height - 100);
+    stroke("black");
+    strokeWeight(5);
+    noFill();
+
+    circle(width / 2, height / 2, diameter);
+
+    fill(255);
+    noStroke();
+
+    let angle = 30;
+    let x = diameter * Math.cos(angle) + height / 2;
+    let y = diameter * Math.sin(angle) + width / 2;
+
+    stroke("red");
+    strokeWeight(25);
+    point(x, y);
 
     let x = noise(t);
-    x = map(x, 0, 1, 100, width - 100);
+    let y = noise(t);
 
-    let y = noise(q);
-    y = map(y, 0, 1, 100, height - 100);
-
-    ellipse(x + random(50), y + random(5), 40, 40);
+    for (let i = 0; i < 50; i++) {
+        x = map(x, 0, 1, 100, width - 100);
+        y = map(y, 0, 1, 100, width - 100);
+        ellipse(x + random(50) * i, y + random(50) * i, 40, 40);
+    }
 
     t += 0.05;
-    q += 5;
 }
 
 function mousePressed() {
